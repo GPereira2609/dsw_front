b1db49947fd1926f595e12f22e90c2fd0503d830
Gabriel <gabriel.pereira2609@gmail.com>
2024-03-16 16:41:20 -0300

    feat/ tela de processos pronta

--- src/pages/NovoProcesso.tsx
+++ src/pages/NovoProcesso.tsx
@@ -1,9 +1,120 @@
 import React, { useState, useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
+import api from '../service/ApiService';
+import 'react-dropdown/style.css';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+
+interface Processo {
+  dataInicio: string | null,
+  cliente: string,
+  tipoprocesso: string,
+  dataFim: string | null,
+  status: string | null
+}
 
 function NovoProcesso() {
-  const [cpfCliente, setCpfCliente] = useState();
+  const [cpfList, setCpfList] = useState();
+  const [tipoProcessoList, setTipoProcessoList] = useState();
+
+  const [selectedOption, setSelectedOption] = useState();
+  const [tipoProcessoSelectedOption, setTipoProcessoSelectedOption] = useState();
+
   const [nomeCliente, setNomeCliente] = useState();
+  const [descricaoTipoProcesso, setDescricaoTipoProcesso] = useState();
+
+  const [dataInicio, setDataInicio] = useState(new Date("01/01/2000"));
+  const [dataFim, setDataFim] = useState("");
+  const [status, setStatus] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const response = api.get("/clientes/cpf")
+      .then((res)=>{
+        const valores = Object.values(res.data);
+        const cpfs = valores.map(item=>item.cpf)
+        setCpfList(cpfs);
+      })
+      .catch((err)=>console.log(err))
+  }, [])
+
+  useEffect(() => {
+    if(selectedOption==="") setNomeCliente("")
+    else {
+    const response = api.get("/clientes/cpf/" + selectedOption)
+      .then((res) => setNomeCliente(res.data[0]["nome"]))
+      .catch((err) => console.log(err))
+    }
+  }, [selectedOption])
+
+  useEffect(()=>{
+    const response = api.get("/tipoProcesso")
+      .then((res) => {
+        const valores = Object.values(res.data);
+        const tipoProcesso = valores.map(item=>item.nome);
+        setTipoProcessoList(tipoProcesso);
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  useEffect(()=>{
+    if(tipoProcessoSelectedOption==="") setDescricaoTipoProcesso("")
+    else {
+      const response = api.get("/tipoProcesso/" + tipoProcessoSelectedOption)
+        .then((res) => setDescricaoTipoProcesso(res.data[0]["descricao"]))
+        .catch((err) => console.log(err))
+    }
+  }, [tipoProcessoSelectedOption])
+
+  const handleSave = () => {
+    try {
+      salvarProcesso();
+    } catch(err) {
+      toast.error("Não foi possível salvar o processo")
+    }
+  }
+
+  console.log(dataInicio)
+  console.log(dataFim)
+  console.log(status)
+
+  const salvarProcesso = () => {
+    let processo: Processo = {
+      "cliente": selectedOption,
+      "tipoprocesso": tipoProcessoSelectedOption,
+      "dataInicio": null,
+      "dataFim": null,
+      "status": null
+    }
+    console.log(processo)
+
+    if(dataInicio) processo["dataInicio"] = dataInicio;
+    if(dataFim) processo["dataFim"] = dataFim;
+    if(status) processo["status"] = status;
+
+    const response = api.post("/processos", processo)
+      .then((res) => {
+        limparPagina();
+        toast.success("Processo salvo");
+        setTimeout(redirect, 3000);
+      })
+      .catch((err) => toast.warning("CPF do cliente, nome do cliente e data inicial são parâmetros obrigatórios"))
+  }
+
+  const redirect = () => {
+    navigate("/processos")
+  }
+
+  const limparPagina = () => {
+    setSelectedOption("");
+    setTipoProcessoSelectedOption("");
+    setDataInicio("");
+    setDataFim("");
+    setStatus("");
+  }
 
   return (
     <div className='h-screen flex'>
@@ -15,15 +126,62 @@ function NovoProcesso() {
           <div className='flex flex-row justify-center items-center'>
             <div className='w-80 h-full flex flex-col justify-center items-center m-6'>
                 <label htmlFor='cpf' className="block text-sm font-bold text-gray-700">CPF do Cliente:</label>
-                <input type='text' id="cpf" name="cpf" value={cpfCliente} placeholder='Digite o CPF do cliente' className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e => setCpfCliente(e.target.value))} />
+                <select value={selectedOption} className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e) => setSelectedOption(e.target.value)}>
+                  <option value="">Selecione um CPF</option>
+                  {cpfList?.map(cpf => (
+                    <option value={cpf}>{cpf}</option>
+                  ))}
+                </select>
             </div>
             <div className='w-80 h-full flex flex-col justify-center items-center m-6'>
                 <label htmlFor='nome' className="block text-sm font-bold text-gray-700">Nome do Cliente:</label>
-                <input type='text' id="nome" name="nome" value={nomeCliente} placeholder='Digite uma descrição para o tipo de processo' className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e => setNomeCliente(e.target.value))} />
+                <input type='text' id="nome" name="nome" value={nomeCliente} placeholder='' className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e => setNomeCliente(e.target.value))} />
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col justify-center items-center'>
+          <h1 className='text-2xl mb-4 text-black text-bold'>Dados do Tipo de Processo</h1>
+          <div className='flex flex-row justify-center items-center'>
+            <div className='w-80 h-full flex flex-col justify-center items-center m-6'>
+              <label htmlFor='tipoProcesso' className='block text-sm font-bold text-gray-700'>Tipo de Processo:</label>
+              <select value={tipoProcessoSelectedOption} className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e) => setTipoProcessoSelectedOption(e.target.value)}>
+                <option value="">Selecione um tipo de processo</option>
+                {tipoProcessoList?.map(nome => (
+                  <option value={nome}>{nome}</option>
+                ))}
+              </select>
+            </div>
+            <div className='w-80 h-full flex flex-col justify-center items-center m-6'>
+              <label htmlFor='descTipoProcesso' className='block text-sm font-bold text-gray-700'>Descrição do tipo de processo:</label>
+              <input type='text' id="descricao" name="descricao" value={descricaoTipoProcesso} placeholder='' className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e) => setDescricaoTipoProcesso(e.target.value)}></input>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col justify-center items-center'>
+          <h1 className='text-2xl mb-4 text-black text-bold'>Dados do Processo</h1>
+          <div className='flex flex-row justify-center items-center'>
+            <div className='w-80 h-full flex flex-col justify-center items-center m-6'>
+              <label htmlFor='data_inicio' className='block text-sm font-bold text-gray-700'>Data de início do processo:</label>
+              <DatePicker selected={dataInicio} onChange={(date) => setDataInicio(date)} className="w-full p-2 border border-gray-300 rounded bg-gray-100 mb-5" />
+              <label htmlFor='status' className='block text-sm font-bold text-gray-700'>Status do processo:</label>
+              <select value={status} className='w-full p-2 border border-gray-300 rounded bg-gray-100' onChange={(e) => setStatus(e.target.value)}>
+                <option value="">Selecione uma opção</option>
+                <option value="iniciado">Iniciado</option>
+                <option value="em andamento">Em andamento</option>
+                <option value="finalizado">Finalizado</option>
+              </select>
+            </div>
+            <div className='w-80 h-full flex flex-col justify-center items-center margin-6'>
+              <label htmlFor='data_fim' className='block text-sm font-bold text-gray-700'>Data de fim do processo:</label>
+              <DatePicker selected={dataFim} onChange={(date) => setDataFim(date)} className="w-full p-2 border border-gray-300 rounded bg-gray-100 mb-10" />
+              <button onClick={handleSave} className='w-80 bg-030637 text-white p-2 rounded cursor-pointer'>+ Salvar processo</button>
             </div>
           </div>
         </div>
       </div>
+      <ToastContainer autoClose={3000} />
     </div>
   )
 }
